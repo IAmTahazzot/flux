7561fbc55e23980a2f6e69f31923db5c2e3ca307
Tahazzot <iamtahazzot@gmail.com>
2024-09-19 16:01:58 +0600

    feat: included shadcn ui for core UI components
    pretty cool ui, I love shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <div className="grid place-items-center h-full font-serif">
-      <div className="text-center space-y-5">
-        <h1 className="text-7xl">Hello from the other side</h1>
-        <p className="text-slate-600">(Wanna see something cool?)</p>
-      </div>
+    <div className='m-4'>
+      <Button>Shadcn UI button</Button>
     </div>
   );
 };
