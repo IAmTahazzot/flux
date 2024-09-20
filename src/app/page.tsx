ccb3c405da33887ccfb74da4192f30c6cee96d1e
Tahazzot <iamtahazzot@gmail.com>
2024-09-20 10:19:43 +0600

    feature: shadcn ui theme tweaked to project ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 const Home = () => {
   return (
-    <div className='m-4'>
-      <Button>Shadcn UI button</Button>
+    <div className="m-5">
+      <Card className="max-w-[400px] mx-auto">
+        <CardHeader>
+          <CardTitle>Something cool</CardTitle>
+          <CardDescription>I would do anything for you.</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <p>I'll always be there by your side.</p>
+          <br />
+
+          <Button>I'm here</Button>
+        </CardContent>
+      </Card>
     </div>
   );
 };
